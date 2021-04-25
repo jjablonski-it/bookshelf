60ca0b75ccf7a1b9e0d1b14e884477340250a352
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-04-25 11:53:33 +0200

    post support

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -1,20 +1,21 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core'
-import * as auth from 'auth-provider'
-import { FullPageSpinner } from 'components/lib'
-import * as React from 'react'
-import { useAsync } from 'utils/hooks'
-import { AuthenticatedApp } from './authenticated-app'
-import { UnauthenticatedApp } from './unauthenticated-app'
-import { client } from './utils/api-client'
+import {jsx} from '@emotion/core'
 
+import * as React from 'react'
+import * as auth from 'auth-provider'
+import {FullPageSpinner} from './components/lib'
+import * as colors from './styles/colors'
+import {client} from './utils/api-client'
+import {useAsync} from './utils/hooks'
+import {AuthenticatedApp} from './authenticated-app'
+import {UnauthenticatedApp} from './unauthenticated-app'
 
 async function getUser() {
   let user = null
 
   const token = await auth.getToken()
   if (token) {
-    const data = await client('me', { token })
+    const data = await client('me', {token})
     user = data.user
   }
 
@@ -22,28 +23,57 @@ async function getUser() {
 }
 
 function App() {
-  // const [user, setUser] = React.useState(null)
-  const { data: user, error, isIdle, isLoading, isSuccess, isError, run, setData: setUser } = useAsync()
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData,
+  } = useAsync()
 
   React.useEffect(() => {
     run(getUser())
   }, [run])
 
-  const login = form => auth.login(form).then(u => setUser(u))
-  const register = form => auth.register(form).then(u => setUser(u))
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
   const logout = () => {
     auth.logout()
-    setUser(null)
+    setData(null)
   }
 
-  if (isLoading || isIdle) return <FullPageSpinner />
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
 
-  return user ? (
-    <AuthenticatedApp user={user} logout={logout} />
-  ) : (
-    <UnauthenticatedApp login={login} register={register} />
-  )
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  if (isSuccess) {
+    return user ? (
+      <AuthenticatedApp user={user} logout={logout} />
+    ) : (
+      <UnauthenticatedApp login={login} register={register} />
+    )
+  }
 }
 
-export { App }
-
+export {App}
